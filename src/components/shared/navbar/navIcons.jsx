b74fc16cdaee324c5e8f33b1871579eb719ddf9c
9ightshade <aniekemeusanga@gmail.com>
2024-07-05 15:02:19 +0100

    navbar

---
+++ src/components/shared/navbar/navIcons.jsx
@@ -0,0 +1,8 @@
+const NavIcons = ()=>{
+
+  return(
+    <div>
+      <img src={search} alt="search icon" /><img src={profile} alt="profile icon" /><img src={fav} alt="fav icon" /><img src={cart} alt="cart icon" />
+    </div>
+  )
+}
---
+++ src/components/shared/navbar/navlinks.jsx
@@ -0,0 +1,35 @@
+const NavLinks = ()=>{
+
+return(
+<ul>
+  <li>
+    <a href="">
+      Home
+    </a>
+  </li>
+  <li>
+    <a href="">
+      Products
+    </a>
+  </li>
+  <li>
+    <a href="">
+      Shop
+    </a>
+  </li>
+  <li>
+    <a href="">
+      About
+    </a>
+  </li>
+  <li>
+    <a href=""
+    >Contact Us
+    </a>
+  </li>
+</ul>
+
+)
+}
+
+export default NavLinks
